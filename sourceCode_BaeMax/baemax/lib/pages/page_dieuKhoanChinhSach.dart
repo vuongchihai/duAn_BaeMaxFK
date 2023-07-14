ee723a23fa6d366402e2016927a48fa9c4d368ca
vuongchihai <haivuong060@gmail.com>
2023-07-14 12:34:34 +0700

    hoan thanh

--- sourceCode_BaeMax/baemax/lib/pages/page_dieuKhoanChinhSach.dart
+++ sourceCode_BaeMax/baemax/lib/pages/page_dieuKhoanChinhSach.dart
@@ -63,7 +63,7 @@ class _dieuKhoanChinhSachPageState extends State<dieuKhoanChinhSachPage> {
                 ),
               ),
               Text(
-                'I. Những Quy Định Chung của BaeMax',
+                'I. Những Quy Định Chung',
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
